9e6c1196d7086a3d538937b4b98392da02841786
visheshsavani <visheshsavani56@gmail.com>
2024-11-26 22:32:44 +0530

    change in backend server.js file relatd to directory

--- Food_Delivery_Web/backend/server.js
+++ Food_Delivery_Web/backend/server.js
@@ -58,7 +58,7 @@ app.get(
   }
 );
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/frontend/dist")));
 
 // // Catch-all route for other requests
 // app.get('*', (req, res) => {
@@ -66,7 +66,7 @@ app.use(express.static(path.join(__dirname, "/frontend/dist")));
 //    });
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "frontend","frontend", "dist", "index.html"));
 });
    
 
